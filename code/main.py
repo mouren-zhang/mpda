8d2c487ec5ec9c377e11270c9752a92cdac49df7
mouren-zhang <1322922002@qq.com>
2022-08-07 22:31:23 +0800

    优化内容
    新增玩家所处世界查看

--- code/main.py
+++ code/main.py
@@ -11,78 +11,6 @@ lis = []
 prmod = 0  # 0仅显示数量 1显示数量+uuid
 data = {}
 
-
-def updata():
-    """
-    更新检查
-    :return:
-    """
-
-    V = 1.20
-    url = 'https://mouren-zhang.github.io/index.json'
-    # url = 'http://127.0.0.1/mpda/index.json'
-
-    headers = {
-        'Connection': 'close',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.134 Safari/537.36 Edg/103.0.1264.71'
-    }
-
-    r = requests.get(url, headers=headers)
-    if r.status_code != 200:
-        return 'no'
-
-    # 版本比对
-    Version = r.json().get('mpda', {}).get('Version')
-
-    if Version == None or Version == '':
-        return 'no'
-
-    if Version != V:
-        # 有变动
-        print('当前版本为', V, '最新版本', Version)
-
-        title = r.json().get('mpda', {}).get('title')
-        print(title)
-
-        text = r.json().get('mpda', {}).get('text')
-        for i in text:
-            print(i)
-
-        print('#' * 10)
-    return 'ok'
-
-
-def len_uuid(filename):
-    """
-    从字符串中提取uuid
-    :param filename: str
-    :return: uuid
-    """
-    return re.compile(r'playerdata\\(.*).dat', re.MULTILINE).findall(filename)[0]
-
-
-def add_w():
-    """
-    获取全部数据
-    :return: dict
-    """
-
-    global lis
-    # 文件路径
-
-    ml = r'playerdata'
-
-    # 获取路径下每个文件的文件路径加入列表
-    for filename in glob.glob(ml + '\*.dat'):
-
-        # 判断文件名长度是否合法 uuid长度不合法的就跳过
-        if len(len_uuid(filename)) != 36:
-            continue
-        # 合法文件加入列表
-        lis.append(filename)
-    print('加载数据', len(lis), '个')
-
-
 def main():
     """
     主函数
@@ -136,9 +64,19 @@ def main():
             elif nbtfile.get("TAG_Int('playerGameType')") == '3':
                 playerGameType='旁观'
 
+
+            world = nbtfile.get("TAG_String('Dimension')")
+            if world == 'minecraft:overworld':
+                world = '主世界'
+            elif world == 'minecraft:the_nether':
+                world = '下界(地狱)'
+            elif world == 'minecraft:the_end':
+                world = '末地'
+
             d = {
                 '游戏模式':playerGameType,
-                '经验等级':nbtfile.get("TAG_Int('XpLevel')")
+                '经验等级':nbtfile.get("TAG_Int('XpLevel')"),
+                '所处世界':world
             }
             # print(len_uuid(i))
 
@@ -154,6 +92,80 @@ def main():
 
     return data
 
+
+
+def updata():
+    """
+    更新检查
+    :return:
+    """
+
+    V = 1.20
+    url = 'https://mouren-zhang.github.io/index.json'
+    # url = 'http://127.0.0.1/mpda/index.json'
+
+    headers = {
+        'Connection': 'close',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.134 Safari/537.36 Edg/103.0.1264.71'
+    }
+
+    r = requests.get(url, headers=headers)
+    if r.status_code != 200:
+        return 'no'
+
+    # 版本比对
+    Version = r.json().get('mpda', {}).get('Version')
+
+    if Version == None or Version == '':
+        return 'no'
+
+    if Version > V:
+        # 有变动
+        print('当前版本为', V, '最新版本', Version)
+
+        title = r.json().get('mpda', {}).get('title')
+        print(title)
+
+        text = r.json().get('mpda', {}).get('text')
+        for i in text:
+            print(i)
+
+        print('#' * 10)
+    return 'ok'
+
+
+def len_uuid(filename):
+    """
+    从字符串中提取uuid
+    :param filename: str
+    :return: uuid
+    """
+    return re.compile(r'playerdata\\(.*).dat', re.MULTILINE).findall(filename)[0]
+
+
+def add_w():
+    """
+    获取全部数据
+    :return: dict
+    """
+
+    global lis
+    # 文件路径
+    ml = r'C:\Users\zhang\Desktop\玩家存档分析\playerdata'
+    # ml = r'playerdata'
+
+    # 获取路径下每个文件的文件路径加入列表
+    for filename in glob.glob(ml + '\*.dat'):
+
+        # 判断文件名长度是否合法 uuid长度不合法的就跳过
+        if len(len_uuid(filename)) != 36:
+            continue
+        # 合法文件加入列表
+        lis.append(filename)
+    print('加载数据', len(lis), '个')
+
+
+
 if __name__ == '__main__':
     try:
         if updata() == 'no':
@@ -163,7 +175,12 @@ if __name__ == '__main__':
     
     print('使用须知\n请不要用本程序去检索热文件，若因为使用不当造成的由使用者承担')
 
-    print('0.统计模式\n1.筛查模式')
+    pr_dt = {
+        '0':'统计模式',
+        '1':'筛查模式'
+    }
+    for k,v in pr_dt.items():
+        print(k,v)
     a = input('$:')
     # a = '0'
     if a == '0':
